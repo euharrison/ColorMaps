9d68f437fa99eef64b3b39de0216a953c4949af1
Harrison <Harrison@Harrisons-MacBook-Pro.local>
2013-11-22 01:43:54 -0200

    save image with separate form for faster develop

--- saveimage.php
+++ saveimage.php
@@ -1,9 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <title>Google Maps Colors</title>
+
+    <style type="text/css">
+    
+    body {
+      margin: 20px;
+      font-family: Arial, sans-serif;
+    }
+
+    </style>
+
+  </head>
+<body>
+
+
+<form action="saveimage.php" method="get">
+  Coordenadas: <input type="text" name="coords" value="-22.903331,-43.173923">
+  <input type="submit">
+</form>
+
+
 <?php
 
-$coords = $_GET['coords'];
-$url = 'http://maps.googleapis.com/maps/api/staticmap?center='.$coords.'&zoom=17&size=200x200&sensor=false&maptype=satellite';
-$img = 'img/gmaps/'.$coords.'.png';
+if (isset($_GET['coords'])) 
+{
+	$coords = $_GET['coords'];
+	$url = 'http://maps.googleapis.com/maps/api/staticmap?center='.$coords.'&zoom=17&size=200x200&sensor=false&maptype=satellite';
+	$img = 'img/gmaps/'.$coords.'.png';
 
-copy($url, $img);
+	$success = copy($url, $img);
+
+	if ($success) {
+		echo '<br><img src="'.$url.'"><br><br>Imagem salva com sucesso. ['.$coords.']';
+	} else {
+		echo '<br><img src="'.$url.'"><br><br>Erro ao salvar a imagem. ['.$coords.']';
+	}
+}
 
 ?>
+
+
+<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="js/color-thief.js"></script>
+<script type="text/javascript" src="js/color-converter.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+
+
+  </body>
+</html>
