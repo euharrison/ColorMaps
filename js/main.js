432f26a17690a9afe4f932ce819c8b82196ce123
Harrison <Harrison@Harrisons-MacBook-Pro.local>
2013-11-22 01:44:10 -0200

    show 10 spots at same time

--- js/main.js
+++ js/main.js
@@ -3,37 +3,17 @@
 // lat = [+80,-80] // +80 = norte, -80 = sul
 // lng = [-180,+180] // -180 = oeste, +180 = leste
 
-$("#search").submit(function(){
-  getColorFrom($("#coords").val());
-  return false;
+
+$("li").each(function() {
+
+  var coord = $("span", this).text();
+
+  var image = $('<img src="img/gmaps/'+coord+'.png">');
+  image.load(onLoadImage);
+
+  $(this).append(image);
+
 })
-
-function getColorFrom(coords)
-{
-  // if you already did a first search by this coord, 
-  // the image will be already saved 
-  // and you don't need to look for it again
-  var isImageAlreadySaved = true;
-
-  if (isImageAlreadySaved) {
-    loadImage("img/gmaps/"+coords+".png");
-  } 
-  else {
-    $.ajax({
-      url: "saveimage.php?coords="+coords,
-    })
-    .done(function() {
-      loadImage("img/gmaps/"+coords+".png");
-    });
-  }
-}
-
-function loadImage(src) 
-{
-  var img = new Image;
-  img.onload = onLoadImage;
-  img.src = src;
-}
 
 function onLoadImage(e) 
 {
@@ -43,15 +23,14 @@ function onLoadImage(e)
 
   colorList.sort(sortColors);
 
-  $("#image").html(img);
-
   var html = "";
   for (var i = 0, total = colorList.length; i < total; i++) {
     var hex = rgbToHex( colorList[i][0], colorList[i][1], colorList[i][2] );
     var hsl = rgbToHsl( colorList[i][0], colorList[i][1], colorList[i][2] );
-    html += '<div class="color" style="background-color:'+hex+';">'+hex+'<br><br>'+hsl.join(' ')+'</div>';
+    html += '<div class="color" style="background-color:'+hex+';">'+hex+'<br><br>'+hsl.join('<br>')+'</div>';
   }
-  $("#color").html(html);
+
+  $(img).parent().append(html);
 }
 
 function sortColors(a,b) 
